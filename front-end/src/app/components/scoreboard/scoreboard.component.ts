bf12d88a9a0a06880a54b1842d7bc31b8a11d632
M0NT4 <montassaramri@insat.u-carthage.tn>
2021-05-03 19:08:56 +0100

    modify scoreboard

--- front-end/src/app/components/scoreboard/scoreboard.component.ts
+++ front-end/src/app/components/scoreboard/scoreboard.component.ts
@@ -13,9 +13,10 @@ export class ScoreboardComponent {
     Score = 0 ;
 
    rows = [
-      {Ranking: 1, Team: 'Za33',Score: 5000, Country: 'TN'},
-      {Ranking: 2, Team: 'Fword',Score: 1, Country: 'FR'},
-      {Ranking: 3, Team: 'Empty_',Score: 0, Country: 'wajdi'},
+      {Ranking: 1, Team: 'MONT4',Score: 5000, Country: 'TN'},
+      {Ranking: 2, Team: 'Fword',Score: 100, Country: 'FR'},
+      {Ranking: 3, Team: 'Empty_',Score: 50, Country: 'TN'},
+      {Ranking: 4, Team: 'Trippy_Bois',Score: 10, Country:'US'}
 
     ];
 
