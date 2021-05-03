a9c49c1bba8f83878f5d0f9967c6d112ef9efc40
M0NT4 <montassaramri@insat.u-carthage.tn>
2021-05-03 17:39:25 +0100

    add scoreboard

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "scoreboard" , component: ScoreboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/components/scoreboard/scoreboard.component.ts
+++ front-end/src/app/components/scoreboard/scoreboard.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
   styleUrls: ['./scoreboard.component.scss']
 })
-export class ScoreboardComponent implements OnInit {
+export class ScoreboardComponent {
 
-  constructor() { }
+    Team = "" ;
+    Ranking = 0 ;
+    Country = "" ;
+    Score = 0 ;
 
-  ngOnInit(): void {
-  }
+   rows = [
+      {Ranking: 1, Team: 'Za33',Score: 5000, Country: 'TN'},
+      {Ranking: 2, Team: 'Fword',Score: 1, Country: 'FR'},
+      {Ranking: 3, Team: 'Empty_',Score: 0, Country: 'wajdi'},
+
+    ];
 
 }
