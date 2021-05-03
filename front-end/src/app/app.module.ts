77d3fa2ff83878361897e60297cbbf2faaaf87bf
M0NT4 <montassaramri@insat.u-carthage.tn>
2021-05-03 17:30:11 +0100

    add scoreboard

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreboardComponent
   ],
   imports: [
     BrowserModule,
