218801de33202feff0ff2eddae9c203d236ebb86
M0NT4 <montassaramri@insat.u-carthage.tn>
2021-05-05 01:08:50 +0100

    modify scoreboard 2

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ChallengesComponent } from './components/challenges/challenges.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ScoreboardService } from './services/scoreboard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScoreboardComponent
+    ScoreboardComponent,
+    HomeComponent,
+    ChallengesComponent,
+    TeamsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    FormsModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ScoreboardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front-end/src/app/components/scoreboard/scoreboard.component.ts
+++ front-end/src/app/components/scoreboard/scoreboard.component.ts
@@ -1,23 +1,20 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ScoreboardService } from '../../services/scoreboard.service';
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
   styleUrls: ['./scoreboard.component.scss']
 })
-export class ScoreboardComponent {
+export class ScoreboardComponent implements OnInit {
+  rows:any;
+  constructor(private scoreboardService:ScoreboardService) { }
+  ngOnInit(): void {
+    this.loadRows();
 
-    Team = "" ;
-    Ranking = 0 ;
-    Country = "" ;
-    Score = 0 ;
-
-   rows = [
-      {Ranking: 1, Team: 'MONT4',Score: 5000, Country: 'TN'},
-      {Ranking: 2, Team: 'Fword',Score: 100, Country: 'FR'},
-      {Ranking: 3, Team: 'Empty_',Score: 50, Country: 'TN'},
-      {Ranking: 4, Team: 'Trippy_Bois',Score: 10, Country:'US'}
-
-    ];
+  }
+  loadRows(){
+    this.scoreboardService.all().subscribe(rows =>this.rows=rows);
+  }
 
 }
