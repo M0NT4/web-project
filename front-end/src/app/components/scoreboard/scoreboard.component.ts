ed04093a1bc3db3ba754a4e70c78a723c654e12b
medalizaabi <mohamedali.zaabi@insat.u-carthage.tn>
2021-05-07 02:40:19 +0200

    add teams + sign in + sign up

--- front-end/src/app/components/scoreboard/scoreboard.component.ts
+++ front-end/src/app/components/scoreboard/scoreboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { ScoreboardService } from '../../services/scoreboard.service';
+import { ScoreboardService } from 'src/app/services/scoreboard.service';
 
 @Component({
   selector: 'app-scoreboard',
@@ -7,14 +7,19 @@ import { ScoreboardService } from '../../services/scoreboard.service';
   styleUrls: ['./scoreboard.component.scss']
 })
 export class ScoreboardComponent implements OnInit {
-  rows:any;
-  constructor(private scoreboardService:ScoreboardService) { }
+
+   rows : any ;
+
+    constructor(private scoreboardservice: ScoreboardService) { }
+
   ngOnInit(): void {
     this.loadRows();
 
   }
-  loadRows(){
-    this.scoreboardService.all().subscribe(rows =>this.rows=rows);
+  loadRows() {
+    this.scoreboardservice.all()
+    .subscribe(rows => this.rows = rows);
   }
+
 
 }
